c7f69d2e5d6a02ed2930f8766e642e0e707a476f
Manos Kazantzidis <manolis.home@gmail.com>
2025-05-23 13:07:00 +0300

    Trying to fix localhost error

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ const accountsRoute = require('./routes/accounts');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Or be specific: 'https://your-frontend.vercel.app'
+}));
+
+
 app.use(express.json());
 app.use('/accounts', accountsRoute);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+  .catch(err => console.error("❌ MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
